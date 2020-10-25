243e682dedabb41209a68daf3be3ff3a7fbbd4ea
Karol <karolsobisz@gmail.com>
2020-10-25 11:11:39 +0100

    add migration

--- coronavirusproject/hello_world/models.py
+++ coronavirusproject/hello_world/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 class Person(models.Model):
+    sexs = (
+        (1, "male"),
+        (2, "female"),
+        (3, "other"))
     age = models.IntegerField()
     height = models.IntegerField()
     name = models.CharField(max_length=100)
     mass = models.IntegerField()
-    sex = models.CharField(max_length=10)
+    sex = models.CharField(max_length=1, choices= sexs)
+    email = models.EmailField(blank=True, null=True)
+    born_date = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+def __str__(self):
+    return f"Osoba: {self.name}, age:{self.age}"
