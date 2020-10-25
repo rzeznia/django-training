a2169152d5947a2ccfd33de021e089fca8e2b3b3
rzeznia <marcin.gd3@gmail.com>
2020-10-25 09:53:48 +0100

    admin

--- coronavirusproject/hello_world/models.py
+++ coronavirusproject/hello_world/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    age = models.IntegerField()
+    height = models.IntegerField()
+    name = models.CharField(max_length=100)
+    mass = models.IntegerField()
+    sex = models.CharField(max_length=10)
